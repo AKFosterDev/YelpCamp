56b6d05e872425a2e52291848a8f443637b37a14
Amanda Foster <akfoster2015@gmail.com>
2020-01-14 15:26:39 +0000

    Commit YelpCamp v4

---
+++ v4/app.js
@@ -0,0 +1,129 @@
+var express 	= require("express"),
+	app 		= express(),
+	bodyParser 	= require("body-parser"),
+	mongoose 	= require("mongoose"),
+	Campground 	= require("./models/campground"),
+	Comment		= require("./models/comment"),
+	seedDB		= require("./seeds");
+
+
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+
+mongoose.connect("mongodb://localhost:27017/yelp_camp_v4", {useNewUrlParser: true});
+				 
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+seedDB();
+
+// =======================================================
+// ROUTES	
+// =======================================================
+
+app.get("/", function(req, res){
+	res.render("landing");
+});
+
+
+// INDEX - Show all campgrounds
+app.get("/campgrounds", function(req, res){
+// 	Get all campgrounds from DB
+	Campground.find({}, function(err, allcampgrounds){
+		if(err){
+			console.log(err);
+		} else {
+			res.render("campgrounds/index", {campgrounds: allcampgrounds});
+		}
+	});
+});
+
+
+// CREATE - add new campground to db
+app.post("/campgrounds", function(req, res){
+	// 	get data from form and add to campgrounds array
+	var name = req.body.name;
+	var image = req.body.image;
+	var desc = req.body.description;
+	var newCampground = {name: name, image: image, description: desc};
+	// Create a new campground and save to DB
+	Campground.create(newCampground, function(err, newlyCreated){
+		if(err){
+			console.log(err);
+		} else {
+			// 	redirect back to campgrounds page
+			res.redirect("/campgrounds");
+		}
+	});
+});
+
+
+// NEW - show form to create new campground
+app.get("/campgrounds/new", function(req, res){
+	res.render("campgrounds/new");
+});
+
+// SHOW - show info for one campground
+app.get("/campgrounds/:id", function(req, res){
+    //find the campground with provided ID
+    Campground.findById(req.params.id).populate("comments").exec( function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else {
+			console.log(foundCampground);
+            //render show template with that campground
+            res.render("campgrounds/show", {campground:foundCampground});
+        }
+    });
+});
+
+// =======================================================
+// COMMENTS ROUTES	
+// =======================================================
+
+app.get("/campgrounds/:id/comments/new", function(req, res){
+    // find campground by id
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        } else {
+             res.render("comments/new", {campground: campground});
+        }
+    })
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+   //lookup campground using ID
+   Campground.findById(req.params.id, function(err, campground){
+       if(err){
+           console.log(err);
+           res.redirect("/campgrounds");
+       } else {
+        Comment.create(req.body.comment, function(err, comment){
+           if(err){
+               console.log(err);
+           } else {
+               campground.comments.push(comment);
+               campground.save();
+               res.redirect('/campgrounds/' + campground._id);
+           }
+        });
+       }
+   });
+   //create new comment
+   //connect new comment to campground
+   //redirect campground show page
+});
+
+// =======================================================
+// SERVER	
+// =======================================================
+	
+app.listen(3000, function(){
+	console.log("The YelpCamp server listening on port 3000");
+});
